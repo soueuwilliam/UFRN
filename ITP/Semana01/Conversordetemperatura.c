f680e344441c08beda60001c5ff24f89edd7b074
William campos Silva <39564940+soueuwilliam@users.noreply.github.com>
2022-04-03 01:31:58 +0000

    Envio dá atividade de Conversão

--- ITP/Semana01/Conversordetemperatura.c
+++ ITP/Semana01/Conversordetemperatura.c
@@ -12,19 +12,38 @@ int main(){
             //para transformar em fahrenheit
             if(temperaturaconveter == 'F'){
                 printf("%.2f F\n",(num*1.8) + 32);
+              
             }
+              
             //Para transformar em kelvin 
             else{
                 printf("%.2f k\n",num + 273.15);
+              
             }
-            break;
+        break;
         //caso o valor atual ofr fahrenheit    
         case 'F':
             //para trasformar em celsius
-            if(temperaturaaconverter == 'C'){
+            if(temperaturaconveter == 'C'){
+              printf("%.2f C\n",(num - 32)/1.8);
               
             }
-
+            //Para transformar em kelvim
+            else{
+              printf("%.2f K\n",((num - 32)/1.8)+273.15);
+              
+            }
+        break;
+        //Caso o valor atual for kelvin
+        case 'K':
+          //para transformar em celsius
+          if(temperaturaconveter == 'C'){
+            printf("%.2f C\n",num - 273.15);
+          }
+          else{
+            printf("%.2f F\n", 1.8*num - (523,67));
+          }
+        break;
     }
     return 0;
 }
