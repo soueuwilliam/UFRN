c606c223f0372206918d3e6311d238def1af49aa
soueuwilliam <camposwilliam23@gmail.com>
2022-04-04 21:04:54 -0300

    Começo da questão de Binário

--- ITP/Semana01/Numbinario.c
+++ ITP/Semana01/Numbinario.c
@@ -2,27 +2,15 @@
 
 int main(){
     //declarando variaveis
-    int decimal;
+    int decimal,binario = 0;
+    int resto ;
     //recebendo entrada
     scanf("%d", &decimal);
     //calculo de binario
     while (decimal != 0 )
     {
-      printf("%d", decimal%2);
-      decimal = decimal/2;
+     
     }
-    
-
+    printf("%d",binario);
     return 0;
 }
-/*
-int decimal_binario(int decimal){
-  int binario = 0;
-  int resto, i = 1;
-  while(decimal != 0) {
-    resto = decimal % 2;
-    decimal = decimal / 2;
-    binario = binario + (resto * i);
-    i = i * 10;
-  }
-*/
