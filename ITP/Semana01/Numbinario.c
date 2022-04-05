94652fa1c79f1ab5d85662a4ead7f3cc4dd3bd9c
soueuwilliam <camposwilliam23@gmail.com>
2022-04-05 09:55:41 -0300

    Adição da questão de número Binário

--- ITP/Semana01/Numbinario.c
+++ ITP/Semana01/Numbinario.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 
 int main(){
-    //declarando variaveis
-    int decimal,binario = 0;
-    int resto ;
+    //declarando variavel
+    int binario[8] = {0};
+    int decimal = 0,index = 7;
+    int i;
+   
     //recebendo entrada
     scanf("%d", &decimal);
-    //calculo de binario
-    while (decimal != 0 )
-    {
-     
+    
+    if(decimal<0 || decimal>255){
+        printf("Número %d não suportado!!",decimal);
+        return 0;
     }
-    printf("%d",binario);
+    //calculo de binario
+    while ( decimal!=0 )
+    {
+        binario[index] = decimal%2;
+        decimal = decimal/2;
+        index-=1;
+        
+    }
+    for (i = 0 ; i <8;i++){
+        printf("%d",binario[i]);
+    }
     return 0;
 }
