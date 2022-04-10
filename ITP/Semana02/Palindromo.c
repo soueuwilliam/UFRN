464aa743d577fa4ac264863524819434d18e6b96
soueuwilliam <camposwilliam23@gmail.com>
2022-04-09 21:55:15 -0300

    Finalização da questão do X com letra ,limpando a questão do palindromo

--- ITP/Semana02/Palindromo.c
+++ ITP/Semana02/Palindromo.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    int num,aux = 0,teste;
-    scanf("%d",&num);
-    while (num%10 != 0)
-    {
-        printf("%d\n",num);
-        aux = aux + (num/10);
-        num /= 10;    
-    }
-    printf("\naux :%d",aux);
+    
     return 0;
 }
--- ITP/Semana02/xcomletra.c
+++ ITP/Semana02/xcomletra.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int qtd,i;
+    int qtd,linha,coluna;
     char letra;
     scanf("%d %c",&qtd ,&letra);
-    for(i = 1;i<qtd ;i++){
-        
+
+    for(linha = 1;linha< qtd;linha++){
+        for(coluna =1;coluna<qtd;coluna++){
+            if((coluna == linha) || (coluna == qtd - linha)){
+                printf("%c", letra);
+            }
+            else{
+                printf(" ");
+            }   
+        }
+        printf("\n");
     }
     return 0;
-
 }
