a59ae361a8ffe7e81ba526f816271df86f8d1ea1
soueuwilliam <camposwilliam23@gmail.com>
2022-04-28 16:47:31 -0300

    Questão 1 finalizada ps.: Juca eu te odeio

--- ITP/Semana03/1-ConversordeBases.c
+++ ITP/Semana03/1-ConversordeBases.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
-
 int Conversao(int value,int base){
-    if(value == -1){
-        printf("Programa encerrado !!");
-        return 1;
-    }
-    else{
         int resul = 0;
         int temp = value;
+        int mul = 1;
         while (temp>0)
         {
-            resul= (resul*10) + (temp%base);
-            temp /= 2;
+            resul= resul + (temp%base)*mul;
+            mul = mul*10;
+            temp /= base;
         }
-        printf("Numero %d(10) na base (%d)= %d(%d)\n",value,base,resul,base);
+        printf("Numero %d(10) na base %d = %d(%d)\n",value,base,resul,base);
         return 0; 
-    }
-
 }
+
 int main(){
     int value,base;
-    while (Conversao == 0)
+    do
     {
-        scanf("%d %d",&value, &base);
+        scanf("%d",&value);
+        if(value == -1){
+            printf("Programa Encerrado!");
+            break;
+        }
+        scanf("%d",&base);
         Conversao(value,base);
-    }
+    
+        
+    } while (value != -1);
+    
     return 0;
 }
