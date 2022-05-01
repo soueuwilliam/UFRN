42a6efc104c8ecac10bafcad44418a39ecb33bd7
soueuwilliam <camposwilliam23@gmail.com>
2022-05-01 12:54:50 -0300

    Finalização da questão Separar vetores

--- ITP/Semana04/1-seraparavetores.c
+++ ITP/Semana04/1-seraparavetores.c
@@ -4,40 +4,67 @@ int main(){
   int mul3[20] = {0};
   int mul4[20] = {0};
   int semmul[20] = {0};
-  int i = 0;
-  int j =0,k = 0,l =0;
+  int i = 0,j = 0,n3 = 0,n4 = 0,n0 = 0;
   int num;
-  for(i = 0; i<20;i++){
-    scanf("%d",&num);
-    if(num%3 == 0){
-      mul3[j] = num;
-      j++;
+
+  for(i;i<20;i++){
+    scanf("%d", &num);
+    if(num%3 == 0 && num%4 == 0){
+      mul3[n3] = num;
+      n3++;
+      mul4[n4] = num;
+      n4++;
+    }
+    else if(num%3 == 0){
+      mul3[n3] = num;
+      n3++;
     }
     else if(num%4 == 0){
-      if(num%4 == 0 && num%3 ==0){
-        mul3[j] = num;
-        j++;
-        mul4[k] = num;
-      
-      }
-      mul4[k] = num;
-      k++;
+      mul4[n4] = num;
+      n4++;
     }
     else{
-      semmul[l] = num;
-      l++;
+      semmul[n0] = num;
+      n0++;
+    }
+  }
+
+  //Printando os vetores que são multiplos de 3 
+  printf("a)");
+  for (i = 0; i < 20; i++)
+  {
+    if(mul3[i] != 0){ 
+      printf("%d",mul3[i]);
+      if(mul3[i+1] != 0){
+        printf(",");
+      }
+    }
+  }
+  printf("\n");
+  //printando os vetores que são multiplos de 4 
+  printf("b)");
+  for (i = 0; i < 20; i++)
+  {
+    if(mul4[i] != 0){ 
+      printf("%d",mul4[i]);
+      if(mul4[i+1] != 0){
+          printf(",");
+      }
     }
     
   }
-  for(int i = 0; i<j;i++){
-    printf("%d,",mul3[i]);
-  }
   printf("\n");
-  for(int i = 0; i<k;i++){
-    printf("%d,",mul4[i]);
+  //Printando o vetor dos nuemros que não são multiplos de 3 e 4 
+  printf("c)");
+  for (i = 0; i < 20; i++)
+  { 
+    if(semmul[i] != 0){
+      printf("%d",semmul[i]);
+      if(semmul[i+1] !=0){
+        printf(",");
+      }
+    }
+    
   }
-  printf("\n");
-  for( int i = 0; i<l;i++){
-    printf("%d,",semmul[i]);
-  }
+
 }
