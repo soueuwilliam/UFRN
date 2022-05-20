d7aabc929ef787a35b7d97dbdc0c92b38e5a990b
soueuwilliam <camposwilliam23@gmail.com>
2022-05-20 16:57:26 -0300

    Questões

---
+++ ITP/Semana06_Strings/Apontadordesubstrings.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+    char textoA[41] = {0};
+    char textoB[41] = {0};
+    int i,j;
+    int contador;
+
+    fgets(textoA,40,stdin);
+    fgets(textoB,40,stdin);
+
+    
+     printf("Repetição: %d",contador);
+     printf("Posições:");
+
+    return 0;
+}
---
+++ ITP/Semana06_Strings/Contadordepalavras.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int qtdcaracteres = 100;
+    char texto[qtdcaracteres];
+    
+        
+    fgets(texto,qtdcaracteres-1,stdin);
+    
+    
+    return 0;
+}
