0d16ce88454c323bf7fddb93c3ac4e22f1c62d7e
Joao Sousa <>
2022-07-21 19:26:52 +0000

    jogo da forca com dois jogadores

--- ITP/Projeto/Jogodaforca.c
+++ ITP/Projeto/Jogodaforca.c
@@ -1,214 +1,190 @@
 #include <stdio.h>
 #include <string.h>
-#include <windows.h>
-#include <locale.h>
+#include <ctype.h> //para usar a função tolower();
+#include <time.h> //gerar numeros aleatorios
 
-
-
-int Menu(){
-    //Titulo
-    
-    int escolha = 0;
-    while(1){
-        printf(" ---------------------------\n");
-        printf("|       JOGO DA FORCA       |\n");
-        printf(" ---------------------------\n");
-        printf("Escolha as opções abaixo\n");
-        printf("1 - Jogar contra o Computador\n");
-        printf("2 - Jogar contra outra pessoa\n");
-        printf("3 - Sobre o jogo\n");
-        scanf("%d", &escolha);
-        
-        switch (escolha){
-        
-        case 1:
-            return 1;
-            break;
-        case 2:
-            return 2;
-            break;
-        case 3:
-            return 3;
-            break;
-        
-        default:
-            printf("Comando invalido ");
-            Sleep(1000);
-            
-            limpatela();
-        }
-    }
-}
 
 void forca(int estado){
-
-    switch (estado)
-    {
-    case 0:
-        printf("\n-------------------");
-        printf("\n|                 |");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n-------------------\n");
-        break;
-    case 1:
-        printf("\n-------------------");
-        printf("\n|                 |");
-        printf("\n|                 0");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n-------------------\n");
-        break;
-    case 2:
-        printf("\n-------------------");
-        printf("\n|                 |");
-        printf("\n|                 0");
-        printf("\n|                 |");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n-------------------\n");
-        break;
-    case 3:
-        printf("\n-------------------");
-        printf("\n|                 |");
-        printf("\n|                 0");
-        printf("\n|                /|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n-------------------\n");
-        break;
-    case 4:
-        printf("\n-------------------\n");
-        printf("\n|                 |");
-        printf("\n|                 0");
-        printf("\n|                /|\\");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n-------------------\n");
-        break;
-    case 5:
-        printf("\n-------------------");
-        printf("\n|                 |");
-        printf("\n|                 0");
-        printf("\n|                /|\\");
-        printf("\n|                /");
-        printf("\n|");
-        printf("\n|");
-        printf("\n-------------------\n");
-        break;
-    case 6:
-        printf("\n-------------------");
-        printf("\n|                 |");
-        printf("\n|                 0");
-        printf("\n|                /|\\");
-        printf("\n|                / \\");
-        printf("\n|");
-        printf("\n|");
-        printf("\n|");
-        printf("\n Perdeu o game|");
-        printf("\n-------------------\n");
-        break;
-    }
-} 
-
-void limpatela(){
-    system("cls");
+  if(estado==0){
+  printf("\n----------");
+  printf("\n|        |");
+  printf("\n|");
+  printf("\n|");
+  printf("\n|");
+  printf("\n|");
+  printf("\n|");
+  printf("\n____________");
+  printf("\n////////////\n\n");
+  }
+  else if(estado==1){
+  printf("\n----------");
+  printf("\n|        |");
+  printf("\n|        0");
+  printf("\n|");
+  printf("\n|");
+  printf("\n|");
+  printf("\n|");
+  printf("\n____________");
+  printf("\n////////////\n\n");
+  }
+  else if(estado==2){
+  printf("\n----------");
+  printf("\n|        |");
+  printf("\n|        0");
+  printf("\n|        |");
+  printf("\n|");
+  printf("\n|");
+  printf("\n|");
+  printf("\n____________");
+  printf("\n////////////\n\n");
+  }
+  else if(estado==3){
+  printf("\n----------");
+  printf("\n|        |");
+  printf("\n|        0");
+  printf("\n|       /|");
+  printf("\n|");
+  printf("\n|");
+  printf("\n|");
+  printf("\n____________");
+  printf("\n////////////\n\n");
+  }
+  else if(estado==4){
+  printf("\n----------");
+  printf("\n|        |");
+  printf("\n|        0");
+  printf("\n|       /|\\");
+  printf("\n|");
+  printf("\n|");
+  printf("\n|");
+  printf("\n____________");
+  printf("\n////////////\n\n");
+  }
+  else if(estado==5){
+  printf("\n----------");
+  printf("\n|        |");
+  printf("\n|        0");
+  printf("\n|       /|\\");
+  printf("\n|       /");
+  printf("\n|");
+  printf("\n|");  
+  printf("\n____________");
+  printf("\n////////////\n\n");
+  }
+  else if(estado==6){
+  printf("\n----------");
+  printf("\n|        |");
+  printf("\n|        0");
+  printf("\n|       /|\\");
+  printf("\n|       / \\");
+  printf("\n|");
+  printf("\n| GAME OVER!");
+  printf("\n____________");
+  printf("\n////////////\n\n");
+  }
+  else if(estado==7){
+  printf("\n----------");
+  printf("\n|        |");
+  printf("\n|");
+  printf("\n|");
+  printf("\n|       \\0");
+  printf("\n|        |\\");
+  printf("\n|       / \\");
+  printf("\n____________");
+  printf("\n////////////\n\n");
+  }
 }
 
+int main(void) {
 
-int main(){
-
-
-    if (Menu() == 2){
-        int erros = 0;
-        char p_sec[100];
-        char p_tela[100];
-       
-        
-        printf("JOGADOR 1:\n");
-        printf("Palavra secreta:\n");
-        scanf(" %s",p_sec);
-        printf("A palavra Secreta e: %s ",p_sec);
-        printf("A palavra tem %i caracteres", strlen(p_sec) - 1);
-        
-
-        strcpy(p_tela,p_sec);
-
-        p_tela[strlen(p_tela) - 1] = '\n';
-
-        int i = 0;
-        
-        for(i =0;i< strlen(p_tela);i++){
-            p_tela[i] = '_';
-        }
-
-
-        char letra;
-        
-
-        while (1)
-        {
-            limpatela();
-            //Imprime a forca
-            forca(erros);
-
-            //Imprimir os underlines para cada letra da palavra
-            printf("Adivinhe: ");
-            for(i = 0;i<strlen(p_tela);i++){
-                printf("%c ",p_tela[i]);
-            }
-
-            //recebe a letra
-            printf("\nLetra: ");
-            char letra;
-            scanf("%c", &letra);
-
-            int sera_que_errou = 1; //0 - Não errou 1 - errou
-
-
-            //Se a letra correta atualiza palavra na tela
-            for(i = 0;i<strlen(p_tela);i++){
-                if(letra == p_sec[i]){
-                    p_tela[i] = letra;
-                    sera_que_errou = 0;
-                }
-                
-            }
-            
-            //verifica se o jogador errou a palavra
-            if(sera_que_errou == 1){
-                    erros++;
-            }
-
-            //verifica se ganhou
-            if(strcmp(p_tela,p_sec) == 0){
-                printf(" A palavra e : %s\n",p_tela); 
-                printf("ganhou");
-                break;
-            }
-            
-            //verifica se o jogo acabou
-            //verifica se perdeu
-            if(erros == 6){
-
-                forca(erros);
-                printf("A palavra correta era : %s",p_sec);
-                break;
-            } 
-        }
+  //Para gerar números aleatórios
+  srand((unsigned)time(NULL));
+  
+  char p_sec[100];//palavra secreta
+  printf("---------------------\n");
+  printf("      JOGADOR 1\n");
+  printf("---------------------\n\n");
+  printf("Palavra secreta: ");
+  fgets(p_sec, 100, stdin);
+  //formata a palava secreta em maiusculo
+  for(int i = 0; i<strlen(p_sec); i++){
+    p_sec[i]=toupper(p_sec[i]);
+  }
+  system("clear"); //limpar tela
+    
+  //retira o ultimo caracter de p_sec que esta a mais devido a captura ser com fgets()
+  p_sec[strlen(p_sec)-1]='\0';
+  char p_tela[100];//palavra para tela
+  strcpy(p_tela, p_sec);//copia de p_sec 
+  //substitui letras por '_'
+  for(int i=0;i<strlen(p_tela);i++){
+    p_tela[i]='_';
+  }
+  
+  int erros = 0;
+  int chances = 6;
+  while(1){
+    //imprimir a forca
+    system("clear"); //limpar tela
+    printf("---------------------\n");
+    printf("      JOGADOR 2\n");
+    printf("---------------------\n\n");
+    printf("A palavra secreta tem %lu caracteres.\n", strlen(p_sec));
+    forca(erros);
+    //imprimir os underline/underscores '_' para cada letra da palavra secreta, ou seja, imprimir p_tela
+    printf("\nPalavra secreta: ");
+    for(int i=0;i<strlen(p_tela);i++){
+      printf("%c ", p_tela[i]);
     }
-    return 0;
+    printf("\n\nVocê tem: %i chance(s)", chances);
+    //recebe a letra
+    printf("\nLetra: ");
+    char letra;
+    scanf(" %c", &letra);//o espaço antes do % é por conta do repl.it usado via web...
+    //se letra correta atualiza palavra na tela 
+    //verifica se a letra esta correta
+    int sera_que_errou=1;//1=sim 0=nao
+    for(int i=0;i<strlen(p_tela); i++){
+      if(toupper(letra)==toupper(p_sec[i])){//certo
+        p_tela[i]=toupper(letra);
+        sera_que_errou=0;
+      } 
+    }
+    //senao, incrementa erros
+    if(sera_que_errou==1){
+      erros++;
+      chances--;
+    }
+    //verifica se o jogo acabou
+    //verifica se ganhou
+    //verifica se p_sec é igual a p_tela
+    if(strcmp(p_tela,p_sec)==0){
+      //ENTAO ganhou
+      system("clear"); //limpar tela
+      printf("---------------------\n");
+      printf("      JOGADOR 2\n");
+      printf("---------------------\n\n");
+      printf("VOCÊ VENCEU!!! PARABÉNS!!!\n");
+      forca(7);
+      printf("\nPalavra secreta: ");
+      for(int i=0;i<strlen(p_tela);i++){
+        printf("%c ", p_tela[i]);
+      }
+      break;
+    }
+    //verifica se perdeu
+    if(erros==6){
+      //perdeu
+      system("clear"); //limpar tela
+      printf("---------------------\n");
+      printf("      JOGADOR 2\n");
+      printf("---------------------\n\n");
+      printf("VOCÊ PERDEU!!! :(\n");
+      forca(erros);
+      printf("A palavra secreta era: %s\n", p_sec);
+      break;
+    }
+  }
+
+  return 0;
 }
